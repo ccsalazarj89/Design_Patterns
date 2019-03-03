e8857baf0d7da63f3b563e1f4afb108eba43ba3a
Christian Salazar <ccsalazarj@gmail.com>
2019-03-03 17:09:07 -0500

    it adds more examples

---
+++ Basics/Pointers/main_pointers_string.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    const char * foo = "hello";
+    cout<<&foo<<endl; // This is the pointer to the first value it means 'h'
+    return 0;
+
+}
